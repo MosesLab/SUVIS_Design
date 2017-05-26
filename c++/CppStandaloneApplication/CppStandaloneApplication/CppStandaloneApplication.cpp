e3536653544699fd4d492027868ae8a274e30b69
byrdie <roytsmart@gmail.com>
2017-05-26 15:03:24 -0600

    Adding diffraction grating to C++ zemax model

--- c++/CppStandaloneApplication/CppStandaloneApplication/CppStandaloneApplication.cpp
+++ c++/CppStandaloneApplication/CppStandaloneApplication/CppStandaloneApplication.cpp
@@ -30,6 +30,7 @@ using namespace ZOSAPI;
 using namespace ZOSAPI_Interfaces;
 
 double angle2d(double ax, double ay, double bx, double by);
+void move_polar(ILensDataEditorPtr lde, ILDERowPtr row, double r, double phi);
 void handleError(std::string msg);
 void logInfo(std::string msg);
 void finishStandaloneApplication(IZOSAPI_ApplicationPtr TheApplication);
@@ -81,6 +82,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	double N_d = 2048; // Number of detector pixels in the dispersion direction
 	double m = 1; // spectral order
 	double r_s = 3;		// (mm) Radius of feed optic
+	double h_d = N_d / 2 * d_d;	// Full height of the detector
 
 	double RR = R_g / 2;	// Radius of rowland circle
 
@@ -109,6 +111,7 @@ int _tmain(int argc, _TCHAR* argv[])
 	// Wavelength at center of detector
 	double lambda = (d_g / m) * (sin(alpha) + sin(beta));
 
+	TheSystem->SystemData->Aperture->ApertureValue = w_g;
 
 	// Open the lens data editor
 	ILensDataEditorPtr lde = TheSystem->LDE;
@@ -126,7 +129,21 @@ int _tmain(int argc, _TCHAR* argv[])
 	s_row->ChangeType(s_row->GetSurfaceTypeSettings(SurfaceType_Toroidal));		// Change the surface type to toroidal
 	ISurfaceToroidalPtr s_surf = s_row->SurfaceData;
 	s_surf->RadiusOfRotation = r_s;		// Radius of rotation of the toroid is equal to the radius of the feed optic
+	ISurfaceApertureTypePtr s_apType = s_row->ApertureData->CreateApertureTypeSettings(SurfaceApertureTypes_RectangularAperture);
+	ISurfaceApertureRectangularPtr rect = s_apType->Get_S_RectangularAperture();
+	rect->XHalfWidth = r_s;		// x half width equal to radius of feed optic
+	rect->YHalfWidth = w_g / 2; // y half width equal to radius of grating
+	s_row->ApertureData->ChangeApertureTypeSettings(s_apType);
+	s_row->Material = _bstr_t::_bstr_t("MIRROR");
+	move_polar(lde, s_row, RR - r_s, phi_s);
 
+	// Create the diffraction grating
+	ILDERowPtr g_row = lde->InsertNewSurfaceAt(lde->NumberOfSurfaces - 1);	// Insert the surface before the image surface
+	s_row->ChangeType(s_row->GetSurfaceTypeSettings(SurfaceType_DiffractionGrating));		// Change the surface type to diffraction grating
+	ISurfaceDiffractionGratingPtr g_surf = g_row->SurfaceData;
+	//g_surf->DiffractionOrder = m;	// Image in the diffraction order specified by m
+	//g_surf->LinesPerMicroMeter = 1 / d_g;	// Number of lines per millimeter is also specified as an argument
+	//g_row->SemiDiameter = w_g / 2;
 
 
 
@@ -162,13 +179,13 @@ void move_polar(ILensDataEditorPtr lde, ILDERowPtr row, double r, double phi) {
 	ILDERowPtr cb1_row = lde->InsertNewSurfaceAt(row->SurfaceNumber);	// create first coordinate break at same index as input surface
 	cb1_row->ChangeType(cb1_row->GetSurfaceTypeSettings(SurfaceType_CoordinateBreak));	// change type to coordinate break
 	ISurfaceCoordinateBreakPtr cb1_surf = cb1_row->SurfaceData;	// acquire surface data to access coordinate break methods
-	cb1_surf->TiltAbout_Y = phi;
+	cb1_surf->TiltAbout_Y = phi * 180 / M_PI;
 	cb1_row->Thickness = r;		// Move to the center of the rowland circle
 
-	ILDERowPtr cb2_row = lde->InsertNewSurfaceAt(row->SurfaceNumber);	// create second coordinate break after input surface
+	ILDERowPtr cb2_row = lde->InsertNewSurfaceAt(row->SurfaceNumber + 2);	// create second coordinate break after input surface
 	cb2_row->ChangeType(cb2_row->GetSurfaceTypeSettings(SurfaceType_CoordinateBreak));	// change type to coordinate break
 	ISurfaceCoordinateBreakPtr cb2_surf = cb2_row->SurfaceData;	// acquire surface data to access coordinate break methods
-	cb2_surf->TiltAbout_Y = -phi;
+	cb2_surf->TiltAbout_Y = -phi * 180 / M_PI;
 	cb2_row->Thickness = -r;		// Move to the center of the rowland circle
 
 }
