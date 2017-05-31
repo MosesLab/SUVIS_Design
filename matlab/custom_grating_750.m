d3124f5840a3f7c62fa7cf505cb0a024b60ba827
Charles Kankelborg <kankel@Charless-MacBook-Pro.local>
2017-05-31 00:45:01 -0700

    Added aberration term (1 pixel wide). 750mm and 1500mm options.

--- matlab/custom_grating_750.m
+++ matlab/custom_grating_750.m
@@ -1,12 +1,12 @@
 % Design using a custom grating
-offset = 2.95*pi/180 % Offset detector from normal
+offset = 8.4*pi/180 % Offset detector from normal
 phi_s =  offset + 10*pi/180 % (rad) angular position of slit     on Rowland Circle
 phi_g = pi-offset % (rad) angular position of grating  on Rowland Circle
 phi_d = offset % (rad) angular position of detector on Rowland Circle
-R_g   =   1500 % (mm)  grating radius
-w_g   =    200 % (mm) grating width
+R_g   =    750 % (mm)  grating radius
+w_g   =    100 % (mm) grating width
 d_s   =  15e-3 % (mm) width of slit
-d_g   = 1/1800 % (mm) grating groove period
+d_g   = 1/3600 % (mm) grating groove period
 d_d   =  15e-3 % (mm) detector pixel spacing
 N_d   =   2048 % Number of detector pixels in the dispersion direction
 m     =      1 % spectral order
@@ -40,7 +40,7 @@ xlabel('wavelength (nm)')
 ylabel('effective resolving power')
 print('custom_grating_resolution_s1.pdf','-dpdfwrite')
 
-phi_s = 32*pi/180 % increase to get up to 2000 Å
+phi_s = 59*pi/180 % increase to get up to 2000 Å
 figure(4)
 [lambdas, deltas] = rowland(phi_s, phi_g, phi_d, R_g, w_g, d_s, d_g, d_d, N_d, m);
 deltas *= sqrt(3/2);
